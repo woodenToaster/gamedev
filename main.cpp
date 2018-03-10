c61f142df50efcd0bfe8df09415536d33d5e398c
Chris <litmusik@yahoo.com>
2018-03-10 12:23:08 -0600

    Working on map scrolling

--- main.cpp
+++ main.cpp
@@ -3,8 +3,35 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+int screen_clamp_x(int val) {
+    if (val < 0) {
+        return 0;
+    }
+    else if (val > 8) {
+        return 8;
+    }
+    else {
+        return val;
+    }
+}
+
+int screen_clamp_y(int val) {
+    if (val < 0) {
+        return 0;
+    }
+    else if (val > 6) {
+        return 6;
+    }
+    else {
+        return val;
+    }
+}
 
 int main(int argc, char** argv) {
+    (void)argc;
+    (void)argv;
+
+    // Window
     const int window_width = 640;
     const int window_height = 480;
 
@@ -30,11 +57,14 @@ int main(int argc, char** argv) {
     Uint32 start = SDL_GetTicks();
     bool running = true;
 
+    // Sprite sheet
     SDL_Surface *sprite_sheet = IMG_Load("sprites/dude.png");
     const int sprite_sheet_width = 256;
     const int sprite_sheet_height = 256;
-    const int w_increment = sprite_sheet_width / 4;
-    const int h_increment = sprite_sheet_height / 4;
+    const int num_x_sprites = 4;
+    const int num_y_sprites = 4;
+    const int w_increment = sprite_sheet_width / num_x_sprites;
+    const int h_increment = sprite_sheet_height / num_y_sprites;
 
     SDL_Rect sprite_rect;
     sprite_rect.x = 0;
@@ -42,7 +72,7 @@ int main(int argc, char** argv) {
     sprite_rect.w = w_increment;
     sprite_rect.h = h_increment;
 
-    float sprite_speed = 10.0f;
+    int sprite_speed = 10;
 
     SDL_Rect dest_rect;
     dest_rect.x = 0;
@@ -56,13 +86,30 @@ int main(int argc, char** argv) {
         printf("Could not load sprite sheet: %s\n", SDL_GetError());
     }
 
+    // Tiles
     int tile_width = 80;
     int tile_height = 80;
+    int x_tiles_per_screen = window_width / tile_width;
+    int y_tiles_per_screen = window_height / tile_height;
 
-    SDL_Rect tile_rect;
-    tile_rect.w = tile_width;
-    tile_rect.h = tile_height;
+    SDL_Rect tile_to_draw;
+    tile_to_draw.w = tile_width;
+    tile_to_draw.h = tile_height;
 
+    struct TileCoord {
+        int x;
+        int y;
+    };
+
+    TileCoord map_tile_coord = {0, 0};
+
+    SDL_Rect current_tile;
+    current_tile.x = sprite_rect.x;
+    current_tile.y = sprite_rect.y;
+    current_tile.w = tile_width;
+    current_tile.h = tile_height;
+
+    // Map
     const int map_width = 12;
     const int map_height = 10;
 
@@ -79,26 +126,26 @@ int main(int argc, char** argv) {
         {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
     };
 
-    SDL_Rect current_tile;
-    current_tile.x = sprite_rect.x;
-    current_tile.y = sprite_rect.y;
-    current_tile.w = tile_width;
-    current_tile.h = tile_height;
-
+    // Camera
     SDL_Rect camera;
     camera.x = 0;
     camera.y = 0;
     camera.w = window_width;
     camera.h = window_height;
 
+    int y_movement_threshold = y_tiles_per_screen / 2;
+    int x_movement_threshold = x_tiles_per_screen / 2;
+
+    // Main loop
     while(running) {
 
         if (SDL_GetTicks() > start + 1000) {
-            printf("FPS: %d\n", frames);
+            // printf("FPS: %d\n", frames);
             frames = 0;
             start = SDL_GetTicks();
         }
 
+        // Input
         SDL_Event event;
         while(SDL_PollEvent(&event)) {
             switch (event.type) {
@@ -110,43 +157,63 @@ int main(int argc, char** argv) {
                 case SDL_QUIT:
                     running = false;
                 case SDL_KEYDOWN:
-                    if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT) {
+                    if (event.key.keysym.scancode == SDL_SCANCODE_RIGHT ||
+                        event.key.keysym.scancode == SDL_SCANCODE_L) {
                         dest_rect.x += sprite_speed;
                         sprite_rect.y = 2 * h_increment;
                     }
-                    if (event.key.keysym.scancode == SDL_SCANCODE_LEFT) {
+                    if (event.key.keysym.scancode == SDL_SCANCODE_LEFT ||
+                        event.key.keysym.scancode == SDL_SCANCODE_H) {
                         dest_rect.x -= sprite_speed;
                         sprite_rect.y = 1 * h_increment;
                     }
-                    if (event.key.keysym.scancode == SDL_SCANCODE_UP) {
+                    if (event.key.keysym.scancode == SDL_SCANCODE_UP ||
+                        event.key.keysym.scancode == SDL_SCANCODE_K) {
                         dest_rect.y -= sprite_speed;
                         sprite_rect.y = 3 * h_increment;
                     }
-                    if (event.key.keysym.scancode == SDL_SCANCODE_DOWN) {
+                    if (event.key.keysym.scancode == SDL_SCANCODE_DOWN ||
+                        event.key.keysym.scancode == SDL_SCANCODE_J) {
                         dest_rect.y += sprite_speed;
                         sprite_rect.y = 0 * h_increment;
                     }
                     current_tile.x = ((dest_rect.x + (dest_rect.w / 2)) / 80) * 80;
                     current_tile.y = ((dest_rect.y + (dest_rect.h / 2)) / 80) * 80;
+                    map_tile_coord.x = current_tile.x / tile_width;
+                    map_tile_coord.y = current_tile.y / tile_height;
+                    printf("Map tile coord: {%d, %d}\n", map_tile_coord.x, map_tile_coord.y);
             }
         }
 
-        for (int i = 0; i < window_height / tile_height; ++i) {
-            for (int j = 0; j < window_width / tile_width; ++j) {
-                tile_rect.x = j * tile_width;
-                tile_rect.y = i * tile_height;
+        int first_tile_coordinate_x = current_tile.x / tile_width;
+        int first_tile_coordinate_y = current_tile.y / tile_height;
 
-                if (map[i][j]) {
+        int screen_y = screen_clamp_y(map_tile_coord.y - y_movement_threshold);
+        int screen_x = screen_clamp_x(map_tile_coord.x - x_movement_threshold);
+
+        // Draw visible portion of map, tile by tile
+        for (int i = 0; i < y_tiles_per_screen; ++i) {
+            for (int j = 0; j < x_tiles_per_screen; ++j) {
+                tile_to_draw.x = j * tile_width;
+                tile_to_draw.y = i * tile_height;
+
+                // See which tile needs to be draw here. Determined relative
+                // to current tile in map coordinates.
+                int map_x = screen_x + j;
+                int map_y = screen_y + i;
+                int tile = map[map_x][map_y];
+
+                if (tile) {
                     SDL_FillRect(
                         windowSurface,
-                        &tile_rect,
+                        &tile_to_draw,
                         SDL_MapRGB(windowSurface->format, 0, 255, 0)
                     );
                 }
                 else {
                     SDL_FillRect(
                         windowSurface,
-                        &tile_rect,
+                        &tile_to_draw,
                         SDL_MapRGB(windowSurface->format, 0, 0, 255)
                     );
                 }
@@ -158,6 +225,13 @@ int main(int argc, char** argv) {
             }
         }
 
+        // Convert map pixel coordinates of dest_rect to screen pixel coordinates.
+        // if (dest_rect.y > screen_y) {
+        //     dest_rect.y = screen_y;
+        // }
+        // if (dest_rect.x > screen_x) {
+        //     dest_rect.x = screen_x;
+        // }
         SDL_BlitSurface(sprite_sheet, &sprite_rect, windowSurface, &dest_rect);
         SDL_UpdateWindowSurface(window);
 
@@ -165,6 +239,7 @@ int main(int argc, char** argv) {
         frames++;
     }
 
+    // Cleanup
     IMG_Quit();
     SDL_DestroyWindow(window);
     SDL_Quit();
--- shell.bat
+++ shell.bat
@@ -1,4 +1,7 @@
-@echo off
+@REM @echo off
 set "VSCMD_START_DIR=w:\"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set PATH=C:\Users\Chris\dev\SDL2-2.0.8\lib\x64;C:\Users\Chris\dev\SDL2_image-2.0.3\lib\x64;%PATH%
+set PATH=C:\Users\Chris\dev\SDL2-2.0.8\lib\x64;%PATH%
+set PATH=C:\Users\Chris\dev\SDL2_image-2.0.3\lib\x64;%PATH%
+set PATH=C:\Users\Chris\programs\emacs-w64-25.3-O2-with-modules\emacs\bin;%PATH%
+cd /d w:
