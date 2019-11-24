f1c31ea584e1b464a217a20175394fd1d93ee10b
Chris <litmusik@yahoo.com>
2019-11-24 10:17:34 -0600

    Update to SDL-2.0.10

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ if not exist "build" mkdir build
 pushd build
 
 set DEV_PREFIX=D:\dev
-set SDL_PATH=SDL2-2.0.8
+set SDL_PATH=SDL2-2.0.10
 set SDL_MIXER_PATH=SDL2_mixer-2.0.2
 set INCLUDE=%DEV_PREFIX%\%SDL_PATH%\include;%INCLUDE%;
 set INCLUDE=%DEV_PREFIX%\%SDL_MIXER_PATH%\include;%INCLUDE%;
@@ -12,6 +12,7 @@ set INCLUDE=%DEV_PREFIX%\gamedev\stb;%INCLUDE%;
 set LIBPATH=%DEV_PREFIX%\%SDL_PATH%\lib\x64;%LIBPATH%;
 set LIBPATH=%DEV_PREFIX%\%SDL_MIXER_PATH%\lib\x64;%LIBPATH%;
 set LIB=%LIBPATH%;%LIB%;
+set PREPROCESSOR_DEFINES="/DDEVELOPER"
 set COMPILERFLAGS="/DDEBUG /W4 /Gm- /Zi /GR- /nologo /EHa- /MTd /Oi /Od"
 set LINK_LIBS=SDL2.lib SDL2main.lib SDL2_mixer.lib
 
@@ -22,10 +23,10 @@ REM We have to rename it manually for hot reloading to work.
 move gamedev.dll gamedev.dll.old
 
 echo waiting for pdb > lock.tmp
-cl "%COMPILERFLAGS%" ..\src\gamedev.cpp /LD ^
+cl "%PREPROCESSOR_DEFINES%" "%COMPILERFLAGS%" ..\src\gamedev.cpp /LD ^
   /link /incremental:no /opt:ref /pdb:gamedev_%random%.pdb /export:gameUpdateAndRender
 del lock.tmp
-cl "%COMPILERFLAGS%" ..\src\sdl2_gamedev.cpp /Fmsdl2_gamedev.map ^
+cl "%PREPROCESSOR_DEFINES%" "%COMPILERFLAGS%" ..\src\sdl2_gamedev.cpp /Fmsdl2_gamedev.map ^
   /link /incremental:no /opt:ref %LINK_LIBS% /SUBSYSTEM:WINDOWS
 
 popd
--- shell.bat
+++ shell.bat
@@ -1,7 +1,7 @@
 @REM @echo off
 set "VSCMD_START_DIR=w:\"
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-set PATH=D:\dev\SDL2-2.0.8\lib\x64;%PATH%
+set PATH=D:\dev\SDL2-2.0.10\lib\x64;%PATH%
 set PATH=D:\dev\SDL2_mixer-2.0.2\lib\x64;%PATH%
 set PATH=C:\Users\Chris\programs\emacs-w64-25.3-O2-with-modules\emacs\bin;%PATH%
 set PATH=C:\Users\Chris\programs\ag-2018-01-25_2;%PATH%
