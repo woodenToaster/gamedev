9d572e462f1fd576568b66330b1617fb032a5e13
Chris <litmusik@yahoo.com>
2020-02-01 10:54:21 -0600

    Updates to external renderer test

--- renderer/main.cpp
+++ renderer/main.cpp
@@ -37,7 +37,6 @@ enum FitResolutionGate {
 	kOverscan
 };
 
-
 struct Screen
 {
     f32 right;
@@ -92,13 +91,11 @@ Screen computeScreenCoordinates(f32 filmApertureWidth, f32 filmApertureHeight,
     return screen;
 }
 
-//[comment]
 // Compute vertex raster screen coordinates.
 // Vertices are defined in world space. They are then converted to camera space,
 // then to NDC space (in the range [-1,1]) and then to raster space.
 // The z-coordinates of the vertex in raster space is set with the z-coordinate
 // of the vertex in camera space.
-//[/comment]
 void convertToRaster(
 	Vec3 &vertexWorld,
 	const Matrix44 &worldToCamera,
@@ -134,19 +131,19 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 {
     u32 imageWidth = width;
     u32 imageHeight = height;
-    const uint32_t ntris = 3156;
-    const float nearClippingPLane = 1;
-    const float farClippingPLane = 1000;
-    float focalLength = 20; // in mm
+    const u32 ntris = 3156;
+    const f32 nearClippingPLane = 1;
+    const f32 farClippingPLane = 1000;
+    f32 focalLength = 20; // in mm
 
-// 35mm Full Aperture in inches
-    float filmApertureWidth = 0.980f;
-    float filmApertureHeight = 0.735f;
+    // 35mm Full Aperture in inches
+    f32 filmApertureWidth = 0.980f;
+    f32 filmApertureHeight = 0.735f;
 
     const Matrix44 worldToCamera = {
-        0.707107f, -0.331295f, 0.624695f, 0.0f,
-        0.0f, 0.883452f, 0.468521f, 0.0f,
-        -0.707107f, -0.331295f, 0.624695f, 0.0f,
+        0.707107f, -0.331295f, 0.624695f,   0.0f,
+        0.0f,       0.883452f, 0.468521f,   0.0f,
+       -0.707107f, -0.331295f, 0.624695f,   0.0f,
         -1.63871f, -5.747777f, -40.400412f, 1.0f
     };
 
@@ -174,33 +171,24 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 		depthBuffer[i] = farClippingPLane;
 	}
 
-	// [comment]
-	// Outer loop
-	// [/comment]
 	for (uint32_t i = 0; i < ntris; ++i) {
 		Vec3 v0 = vertices[nvertices[i * 3]];
 		Vec3 v1 = vertices[nvertices[i * 3 + 1]];
 		Vec3 v2 = vertices[nvertices[i * 3 + 2]];
 
-		// [comment]
 		// Convert the vertices of the triangle to raster space
-		// [/comment]
 		Vec3 v0Raster, v1Raster, v2Raster;
 		convertToRaster(v0, worldToCamera, l, r, t, b, nearClippingPLane, imageWidth, imageHeight, v0Raster);
 		convertToRaster(v1, worldToCamera, l, r, t, b, nearClippingPLane, imageWidth, imageHeight, v1Raster);
 		convertToRaster(v2, worldToCamera, l, r, t, b, nearClippingPLane, imageWidth, imageHeight, v2Raster);
 
-		// [comment]
 		// Precompute reciprocal of vertex z-coordinate
-		// [/comment]
 		v0Raster.z = 1 / v0Raster.z,
 		v1Raster.z = 1 / v1Raster.z,
 		v2Raster.z = 1 / v2Raster.z;
 
-		// [comment]
 		// Prepare vertex attributes. Divide them by their vertex z-coordinate
 		// (though we use a multiplication here because v.z = 1 / v.z)
-		// [/comment]
 		Vec2 st0 = cow_st[stindices[i * 3]];
 		Vec2 st1 = cow_st[stindices[i * 3 + 1]];
 		Vec2 st2 = cow_st[stindices[i * 3 + 2]];
@@ -220,16 +208,14 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 		}
 
 		// be careful xmin/xmax/ymin/ymax can be negative. Don't cast to uint32_t
-		uint32_t x0 = std::max(int32_t(0), (int32_t)(std::floor(xmin)));
-		uint32_t x1 = std::min(int32_t(imageWidth) - 1, (int32_t)(std::floor(xmax)));
-		uint32_t y0 = std::max(int32_t(0), (int32_t)(std::floor(ymin)));
-		uint32_t y1 = std::min(int32_t(imageHeight) - 1, (int32_t)(std::floor(ymax)));
+		uint32_t x0 = std::max(i32(0), (int32_t)(std::floor(xmin)));
+		uint32_t x1 = std::min(i32(imageWidth) - 1, (int32_t)(std::floor(xmax)));
+		uint32_t y0 = std::max(i32(0), (int32_t)(std::floor(ymin)));
+		uint32_t y1 = std::min(i32(imageHeight) - 1, (int32_t)(std::floor(ymax)));
 
 		float area = edgeFunction(v0Raster, v1Raster, v2Raster);
 
-		// [comment]
 		// Inner loop
-		// [/comment]
 		for (uint32_t y = y0; y <= y1; ++y) {
 			for (uint32_t x = x0; x <= x1; ++x) {
 				Vec3 pixelSample = {x + 0.5f, y + 0.5f, 0.0f};
@@ -242,22 +228,19 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 					w2 /= area;
 					float oneOverZ = v0Raster.z * w0 + v1Raster.z * w1 + v2Raster.z * w2;
 					float z = 1 / oneOverZ;
-					// [comment]
+
 					// Depth-buffer test
-					// [/comment]
 					if (z < depthBuffer[y * imageWidth + x]) {
 						depthBuffer[y * imageWidth + x] = z;
 
 						Vec2 st = st0 * w0 + st1 * w1 + st2 * w2;
 						st *= z;
 
-						// [comment]
 						// If you need to compute the actual position of the shaded
 						// point in camera space. Proceed like with the other vertex attribute.
 						// Divide the point coordinates by the vertex z-coordinate then
 						// interpolate using barycentric coordinates and finally multiply
 						// by sample depth.
-						// [/comment]
 						Vec3 v0Cam = multVecMatrix(worldToCamera, v0);
 						Vec3 v1Cam = multVecMatrix(worldToCamera, v1);
 						Vec3 v2Cam = multVecMatrix(worldToCamera, v2);
@@ -267,13 +250,11 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 
 						Vec3 pt = {px * z, py * z, -z}; // pt is in camera space
 
-						// [comment]
 						// Compute the face normal which is used for a simple facing ratio.
 						// Keep in mind that we are doing all calculation in camera space.
 						// Thus the view direction can be computed as the point on the object
 						// in camera space minus Vec3(0), the position of the camera in camera
 						// space.
-						// [/comment]
 						Vec3 n = crossProduct(v1Cam - v0Cam, v2Cam - v0Cam);
 						n = normalize(n);
 						Vec3 viewDirection = -pt;
@@ -281,10 +262,8 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 
 						float nDotView = std::max(0.f, dotProduct(n, viewDirection));
 
-						// [comment]
 						// The final color is the result of the facing ratio multiplied by the
 						// checkerboard pattern.
-						// [/comment]
 						const int M = 10;
 						float checker = (f32)((fmod(st.x * M, 1.0f) > 0.5f) ^ (fmod(st.y * M, 1.0f) < 0.5f));
 						float c = 0.3f * (1.0f - checker) + 0.7f * checker;
@@ -302,19 +281,14 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
     int pitch = imageWidth * bytesPerPixel;
     for (u32 y = 0; y < imageHeight; ++y)
     {
-        u8 *pixel = row;
+        u32 *pixel = (u32 *)row;
         for (u32 x = 0; x < imageWidth; ++x)
         {
             Vec3u v = frameBuffer[y * imageWidth + x];
-
-            *pixel = (u8)v.z;
-            ++pixel;
-            *pixel = (u8)v.y;
-            ++pixel;
-            *pixel = (u8)v.x;
-            ++pixel;
-            *pixel = 0;
-            ++pixel;
+            // RR GG BB xx
+            // xx BB GG RR
+            // xx RR GG BB
+            *pixel++ = (v.x << 16) | (v.y << 8) | (v.z << 0);
         }
         row += pitch;
     }
@@ -324,6 +298,7 @@ int renderCow(int bytesPerPixel, u32 width, u32 height)
 
 	return 0;
 }
+
 /* SCRATCH A PIXEL */
 
 internal void win32ResizeDIBSection(int width, int height)
