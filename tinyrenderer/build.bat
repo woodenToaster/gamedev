8b66ff382dcedfa013ec423b030e6516ae6a07ab
Chris <litmusik@yahoo.com>
2020-02-29 16:12:40 -0600

    Add tinyrender starting files

---
+++ tinyrenderer/build.bat
@@ -0,0 +1,12 @@
+@echo off
+
+if not exist "build" mkdir build
+pushd build
+
+set COMPILERFLAGS=/DDEBUG /W4 /wd4201 /Gm- /Zi /GR- /nologo /EHa- /MTd /Oi /Od
+set LINK_LIBS=user32.lib gdi32.lib
+
+cl /EHsc %COMPILERFLAGS% ..\main.cpp %LINK_LIBS%
+
+popd
+if %errorlevel% neq 0 exit /b %errorlevel
