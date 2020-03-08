ad256544be0468d56c3951f130ffbcda56c3bdc6
Chris <litmusik@yahoo.com>
2020-03-07 21:00:17 -0600

    Using buffers to feed vertex attributes

--- opengl/main.c
+++ opengl/main.c
@@ -4,6 +4,7 @@
 
 #include <windows.h>
 #include <GL/gl.h>
+#include "glext.h"
 
 typedef uint8_t u8;
 typedef uint16_t u16;
@@ -17,45 +18,32 @@ typedef float f32;
 typedef double f64;
 typedef int32_t b32;
 
-#define GL_VERTEX_SHADER 0x8B31
-#define GL_FRAGMENT_SHADER 0x8B30
-#define GL_COMPILE_STATUS 0x8B81
-
-typedef char GLchar;
-
-typedef void (*PFNGLCLEARBUFFERFVPROC)(GLenum buffer, GLint drawbuffer, const GLfloat *value);
-PFNGLCLEARBUFFERFVPROC glClearBufferfv;
-typedef GLuint (*PFNGLCREATESHADERPROC)(GLenum type);
-PFNGLCREATESHADERPROC glCreateShader;
-typedef void (*PFNGLSHADERSOURCEPROC)(GLuint shader, GLsizei count, const GLchar *const*string,
-                                      const GLint *length);
-PFNGLSHADERSOURCEPROC glShaderSource;
-typedef void (*PFNGLCOMPILESHADERPROC) (GLuint shader);
-PFNGLCOMPILESHADERPROC glCompileShader;
-typedef GLuint (*PFNGLCREATEPROGRAMPROC)();
-PFNGLCREATEPROGRAMPROC glCreateProgram;
-typedef void (*PFNGLATTACHSHADERPROC)(GLuint program, GLuint shader);
 PFNGLATTACHSHADERPROC glAttachShader;
-typedef void (*PFNGLLINKPROGRAMPROC)(GLuint program);
-PFNGLLINKPROGRAMPROC glLinkProgram;
-typedef void (*PFNGLDELETESHADERPROC)(GLuint shader);
-PFNGLDELETESHADERPROC glDeleteShader;
-typedef void (*PFNGLCREATEVERTEXARRAYSPROC)(GLsizei n, GLuint *arrays);
-PFNGLCREATEVERTEXARRAYSPROC glCreateVertexArrays;
-typedef void (*PFNGLBINDVERTEXARRAYPROC)(GLuint array);
+PFNGLBINDBUFFERPROC glBindBuffer;
 PFNGLBINDVERTEXARRAYPROC glBindVertexArray;
-typedef void (*PFNGLDELETEVERTEXARRAYSPROC)(GLsizei n, const GLuint *arrays);
-PFNGLDELETEVERTEXARRAYSPROC glDeleteVertexArrays;
-typedef void (*PFNGLDELETEPROGRAMPROC)(GLuint program);
+PFNGLBUFFERSUBDATAPROC glBufferSubData;
+PFNGLCLEARBUFFERFVPROC glClearBufferfv;
+PFNGLCOMPILESHADERPROC glCompileShader;
+PFNGLCREATEBUFFERSPROC glCreateBuffers;
+PFNGLCREATEPROGRAMPROC glCreateProgram;
+PFNGLCREATESHADERPROC glCreateShader;
+PFNGLCREATEVERTEXARRAYSPROC glCreateVertexArrays;
 PFNGLDELETEPROGRAMPROC glDeleteProgram;
-typedef void (*PFNGLUSEPROGRAMPROC)(GLuint program);
-PFNGLUSEPROGRAMPROC glUseProgram;
-typedef void (*PFNGLGETSHADERINFOLOGPROC)(GLuint shader, GLsizei bufSize, GLsizei *length,
-                                          GLchar *infoLog);
+PFNGLDELETESHADERPROC glDeleteShader;
+PFNGLDELETEVERTEXARRAYSPROC glDeleteVertexArrays;
+PFNGLENABLEVERTEXARRAYATTRIBPROC glEnableVertexArrayAttrib;
 PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
-typedef void (*PFNGLGETSHADERIVPROC) (GLuint shader, GLenum pname, GLint *params);
 PFNGLGETSHADERIVPROC glGetShaderiv;
+PFNGLLINKPROGRAMPROC glLinkProgram;
+PFNGLNAMEDBUFFERSTORAGEPROC glNamedBufferStorage;
+PFNGLSHADERSOURCEPROC glShaderSource;
+PFNGLUSEPROGRAMPROC glUseProgram;
+PFNGLVERTEXARRAYATTRIBBINDINGPROC glVertexArrayAttribBinding;
+PFNGLVERTEXARRAYATTRIBFORMATPROC glVertexArrayAttribFormat;
+PFNGLVERTEXARRAYVERTEXBUFFERPROC glVertexArrayVertexBuffer;
+PFNGLVERTEXATTRIB4FVPROC glVertexAttrib4fv;
 
+#define ARRAY_COUNT(arr) (sizeof(arr) / sizeof(arr[0]))
 #define global static
 
 global HDC GlobalDeviceContext;
@@ -124,7 +112,6 @@ LRESULT CALLBACK WindowProc(HWND WindowHandle, UINT Message, WPARAM WParam, LPAR
         {
             MessageBox(NULL, "SetPixelFormat failed", "Error", MB_OK);
             PostQuitMessage(0);
-            return 0;
         }
 
         GlobalRenderingContext = wglCreateContext(GlobalDeviceContext);
@@ -133,25 +120,19 @@ LRESULT CALLBACK WindowProc(HWND WindowHandle, UINT Message, WPARAM WParam, LPAR
         RECT ClientRect = {0};
         GetClientRect(WindowHandle, &ClientRect);
 
-        glEnable(GL_DEPTH_TEST);
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        glShadeModel(GL_FLAT);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(0, 1, 0, 1, -1, 1);
         return 0;
     }
     case WM_SIZE:
     {
-        RECT ClientRect;
-        GetClientRect(WindowHandle, &ClientRect);
+        // RECT ClientRect;
+        // GetClientRect(WindowHandle, &ClientRect);
 
-        int Width = ClientRect.right;
-        int Height = ClientRect.bottom;
-        glViewport(0, 0, (GLsizei)Width, (GLsizei)Height);
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        glOrtho(0, (GLdouble)Width, 0, (GLdouble)Height, -1, 1);
+        // int Width = ClientRect.right;
+        // int Height = ClientRect.bottom;
+        // glViewport(0, 0, (GLsizei)Width, (GLsizei)Height);
+        // glMatrixMode(GL_PROJECTION);
+        // glLoadIdentity();
+        // glOrtho(0, (GLdouble)Width, 0, (GLdouble)Height, -1, 1);
         return 0;
     }
     case WM_DESTROY:
@@ -188,86 +169,174 @@ void *GetAnyGLFuncAddress(const char *name)
 
 static const char *vertexShaderSource[] =
 {
-    "#version 450 core \n",
-    "void main(void) \n",
-    "{ \n",
-    "const vec4 vertices[3] = vec4[3](vec4(0.25, -0.25, 0.5, 1.0),",
-    "                                 vec4(-0.25, -0.25, 0.5, 1.0),",
-    "                                 vec4(0.25, 0.25, 0.5, 1.0));",
-    "gl_Position = vertices[gl_VertexID];",
-    "}",
+    "#version 450 core                                                         \n",
+    // "layout (location = 1) in vec4 my_offset;           \n",
+    // "layout (location = 2) in vec4 color;                                      \n",
+    "layout (location = 1) in vec4 position;                                   \n",
+    // "out vec4 vs_color;                                                        \n",
+    "void main(void)                                                           \n",
+    "{                                                                         \n",
+    // "    const vec4 vertices[3] = vec4[3](vec4(0.25, -0.25, 0.5, 1.0),         \n",
+    // "                                     vec4(-0.25, -0.25, 0.5, 1.0),        \n",
+    // "                                     vec4(0.25, 0.25, 0.5, 1.0));         \n",
+    // "    gl_Position = vertices[gl_VertexID] + my_offset; \n",
+    // "    gl_Position = vertices[gl_VertexID];                                  \n",
+    "    gl_Position = position;                                               \n",
+    // "    vs_color = color;                                                     \n"
+    "}                                                                         \n",
+};
+
+static const char *tessControlShaderSource[] =
+{
+    "#version 450 core                                       \n",
+    "layout (vertices = 3) out;                              \n",
+    "void main(void)                                         \n",
+    "{                                                       \n",
+        "if (gl_InvocationID == 0)                           \n",
+        "{                                                   \n",
+            "gl_TessLevelInner[0] = 5.0;                     \n",
+            "gl_TessLevelOuter[0] = 5.0;                     \n",
+            "gl_TessLevelOuter[1] = 5.0;                     \n",
+            "gl_TessLevelOuter[2] = 5.0;                     \n",
+        "}                                                   \n",
+        "gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position; \n",
+    "}                                                       \n",
+};
+
+static const char *tessEvaluationShaderSource[] =
+{
+    "#version 450 core                                            \n",
+    "layout (triangles, equal_spacing, cw) in;                    \n",
+    "void main(void)                                              \n",
+    "{                                                            \n",
+    "    gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position +   \n",
+    "                   gl_TessCoord.y * gl_in[1].gl_Position +   \n",
+    "                   gl_TessCoord.z * gl_in[2].gl_Position);   \n",
+    "}                                                            \n",
+};
+
+static const char *geometryShaderSource[] =
+{
+    "#version 450 core                                 \n",
+    "layout (triangles) in;                            \n",
+    "layout (points, max_vertices = 3) out;            \n",
+    "void main(void)                                   \n",
+    "{                                                 \n",
+        "int i;                                        \n",
+        "for (i = 0; i < gl_in.length(); i++)          \n",
+        "{                                             \n",
+            "gl_Position = gl_in[i].gl_Position;       \n",
+            "EmitVertex();                             \n",
+    "    }                                             \n",
+    "}                                                 \n",
 };
 
 static const char *fragmentShaderSource[] =
 {
-    "#version 450 core \n",
-    "out vec4 color; \n",
-    "void main(void) \n",
-    "{ \n",
-    "    color = vec4(0.0, 0.8, 1.0, 1.0); \n",
-    "}"
+    "#version 450 core                     \n",
+    // "in vec4 vs_color;                     \n",
+    "out vec4 color;                       \n",
+    "void main(void)                       \n",
+    "{                                     \n",
+    // "    color = vs_color;                 \n",
+    "    color = vec4(0.0, 0.8, 0.2, 1.0); \n",
+    "}                                     \n"
 };
+
+GLuint compileShader(GLenum shaderType, const GLchar *shaderSource[], GLsizei numLines)
+{
+    GLuint shader = glCreateShader(shaderType);
+    glShaderSource(shader, numLines, shaderSource, NULL);
+    glCompileShader(shader);
+    GLint compileResult;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &compileResult);
+    if (compileResult != GL_TRUE)
+    {
+        char info[256];
+        GLsizei length;
+        glGetShaderInfoLog(shader, 256, &length, info);
+        OutputDebugString(info);
+        exit(1);
+    }
+
+    return shader;
+}
 
 GLuint compileShaders()
 {
-    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 8, vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    GLint compile_result;
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &compile_result);
-    if (compile_result != GL_TRUE)
-    {
-        char info[256];
-        GLsizei length;
-        glGetShaderInfoLog(vertexShader, 256, &length, info);
-        OutputDebugString(info);
-        exit(1);
-    }
-
-
-    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 6, fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &compile_result);
-    if (compile_result != GL_TRUE)
-    {
-        char info[256];
-        GLsizei length;
-        glGetShaderInfoLog(fragmentShader, 256, &length, info);
-        OutputDebugString(info);
-        exit(1);
-    }
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource,
+                                        ARRAY_COUNT(vertexShaderSource));
+    GLuint tessControlShader = compileShader(GL_TESS_CONTROL_SHADER, tessControlShaderSource,
+                                             ARRAY_COUNT(tessControlShaderSource));
+    GLuint tessEvaluationShader = compileShader(GL_TESS_EVALUATION_SHADER, tessEvaluationShaderSource,
+                                                ARRAY_COUNT(tessEvaluationShaderSource));
+    GLuint geometryShader = compileShader(GL_GEOMETRY_SHADER, geometryShaderSource,
+                                          ARRAY_COUNT(geometryShaderSource));
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource,
+                                          ARRAY_COUNT(fragmentShaderSource));
 
     GLuint program = glCreateProgram();
     glAttachShader(program, vertexShader);
+    glAttachShader(program, tessControlShader);
+    glAttachShader(program, tessEvaluationShader);
+    // glAttachShader(program, geometryShader);
     glAttachShader(program, fragmentShader);
     glLinkProgram(program);
 
     glDeleteShader(vertexShader);
+    glDeleteShader(tessControlShader);
+    glDeleteShader(tessEvaluationShader);
+    glDeleteShader(geometryShader);
     glDeleteShader(fragmentShader);
 
     return program;
 }
 
-#define LOAD_GL_FUNC(name, uppername) (name = (PFNGL##uppername##PROC)GetAnyGLFuncAddress(#name))
+#define LOAD_GL_FUNC(name, uppername) name = (PFNGL##uppername##PROC)GetAnyGLFuncAddress(#name)
 
 void loadOpenGLFunctions()
 {
-    glClearBufferfv = (PFNGLCLEARBUFFERFVPROC)GetAnyGLFuncAddress("glClearBufferfv");
-    glCreateShader = (PFNGLCREATESHADERPROC)GetAnyGLFuncAddress("glCreateShader");
-    glShaderSource = (PFNGLSHADERSOURCEPROC)GetAnyGLFuncAddress("glShaderSource");
-    glCompileShader = (PFNGLCOMPILESHADERPROC)GetAnyGLFuncAddress("glCompileShader");
-    glCreateProgram = (PFNGLCREATEPROGRAMPROC)GetAnyGLFuncAddress("glCreateProgram");
-    glAttachShader = (PFNGLATTACHSHADERPROC)GetAnyGLFuncAddress("glAttachShader");
-    glLinkProgram = (PFNGLLINKPROGRAMPROC)GetAnyGLFuncAddress("glLinkProgram");
-    glDeleteShader = (PFNGLDELETESHADERPROC)GetAnyGLFuncAddress("glDeleteShader");
-    glCreateVertexArrays = (PFNGLCREATEVERTEXARRAYSPROC)GetAnyGLFuncAddress("glCreateVertexArrays");
-    glBindVertexArray = (PFNGLBINDVERTEXARRAYPROC)GetAnyGLFuncAddress("glBindVertexArray");
+    LOAD_GL_FUNC(glClearBufferfv, CLEARBUFFERFV);
+    LOAD_GL_FUNC(glCreateShader, CREATESHADER);
+    LOAD_GL_FUNC(glShaderSource, SHADERSOURCE);
+    LOAD_GL_FUNC(glCompileShader, COMPILESHADER);
+    LOAD_GL_FUNC(glCreateProgram, CREATEPROGRAM);
+    LOAD_GL_FUNC(glAttachShader, ATTACHSHADER);
+    LOAD_GL_FUNC(glLinkProgram, LINKPROGRAM);
+    LOAD_GL_FUNC(glDeleteShader, DELETESHADER);
+    LOAD_GL_FUNC(glCreateVertexArrays, CREATEVERTEXARRAYS);
+    LOAD_GL_FUNC(glBindVertexArray, BINDVERTEXARRAY);
     LOAD_GL_FUNC(glDeleteVertexArrays, DELETEVERTEXARRAYS);
     LOAD_GL_FUNC(glDeleteProgram, DELETEPROGRAM);
     LOAD_GL_FUNC(glUseProgram, USEPROGRAM);
     LOAD_GL_FUNC(glGetShaderInfoLog, GETSHADERINFOLOG);
     LOAD_GL_FUNC(glGetShaderiv, GETSHADERIV);
+    LOAD_GL_FUNC(glVertexAttrib4fv, VERTEXATTRIB4FV);
+    LOAD_GL_FUNC(glCreateBuffers, CREATEBUFFERS);
+    LOAD_GL_FUNC(glNamedBufferStorage, NAMEDBUFFERSTORAGE);
+    LOAD_GL_FUNC(glBufferSubData, BUFFERSUBDATA);
+    LOAD_GL_FUNC(glVertexArrayAttribBinding, VERTEXARRAYATTRIBBINDING);
+    LOAD_GL_FUNC(glVertexArrayVertexBuffer, VERTEXARRAYVERTEXBUFFER);
+    LOAD_GL_FUNC(glVertexArrayAttribFormat, VERTEXARRAYATTRIBFORMAT);
+    LOAD_GL_FUNC(glEnableVertexArrayAttrib, ENABLEVERTEXARRAYATTRIB);
+    LOAD_GL_FUNC(glBindBuffer, BINDBUFFER);
+}
+
+void render(f32 dt, GLuint program)
+{
+    (void)dt;
+    // const GLfloat color[] = {(f32)sinf(dt) * 0.5f + 0.5f, (f32)cosf(dt) * 0.5f + 0.5f, 0.0f, 1.0f};
+    const GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f};
+    glClearBufferfv(GL_COLOR, 0, red);
+    glUseProgram(program);
+    // GLfloat offset_attrib[] = {(f32)sinf(dt) * 0.5f, (f32)cosf(dt) * 0.6f, 0.0f, 0.0f};
+    // glVertexAttrib4fv(1, offset_attrib);
+    // const GLfloat triangleColor[] = {0.0f, 0.8f, 0.2f, 1.0f};
+    // glVertexAttrib4fv(2, triangleColor);
+
+    // glPointSize(5.0f);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glDrawArrays(GL_PATCHES, 0, 3);
 }
 
 int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int CmdShow)
@@ -309,11 +378,27 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int
     glCreateVertexArrays(1, &vertexArrayObject);
     glBindVertexArray(vertexArrayObject);
 
+    GLuint gpuBuffer;
+    glCreateBuffers(1, &gpuBuffer);
+    glNamedBufferStorage(gpuBuffer, 1024 * 1024, NULL, GL_MAP_WRITE_BIT);
+    glBindBuffer(GL_ARRAY_BUFFER, gpuBuffer);
+
+    const f32 triangleVertices[] =
+    {
+        0.25f, -0.25f, 0.5f, 1.0f,
+        -0.25f, -0.25f, 0.5f, 1.0f,
+        0.25f, 0.25f, 0.5f, 1.0f
+    };
+
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(triangleVertices), triangleVertices);
+    glVertexArrayVertexBuffer(vertexArrayObject, 0, gpuBuffer, 0, 4 * sizeof(f32));
+    glVertexArrayAttribFormat(vertexArrayObject, 1, 4, GL_FLOAT, GL_FALSE, 0);
+    glEnableVertexArrayAttrib(vertexArrayObject, 1);
+
+    LARGE_INTEGER start = win32GetTicks();
     if (WindowHandle)
     {
         ShowWindow(WindowHandle, CmdShow);
-        UpdateWindow(WindowHandle);
-        // glEnable(GL_BLEND);
         LARGE_INTEGER ElapsedMicroseconds = {0};
         GlobalRunning = 1;
         while (GlobalRunning)
@@ -333,12 +418,8 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int
                 }
             }
 
-            const GLfloat red[] = {1.0f, 0.0f, 0.0f, 1.0f};
-            glClearBufferfv(GL_COLOR, 0, red);
-
-            glUseProgram(program);
-            glPointSize(40.0f);
-            glDrawArrays(GL_TRIANGLES, 0, 3);
+            f32 seconds_elapsed = win32GetSecondsElapsed(start, currentTick);
+            render(seconds_elapsed, program);
 
             SwapBuffers(GlobalDeviceContext);
 
@@ -361,9 +442,9 @@ int WINAPI WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, PSTR CmdLine, int
                     }
                 }
             }
-            char dt_str[32];
-            snprintf(dt_str, 32, "MS per frame: %d\n", dt);
-            OutputDebugString(dt_str);
+            // char dt_str[32];
+            // snprintf(dt_str, 32, "MS per frame: %f\n", dt);
+            // OutputDebugString(dt_str);
         }
     }
     else
